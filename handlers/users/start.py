922042dc2887d5dfc3bb1cb2865767bf4b5c19dc
vankaspr <msvankaspr@gmail.com>
2025-08-13 19:38:40 +0500

    📌 add handler for return to home

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,12 +1,25 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 from keyboards import menu_kb
+from settings.middlewares import logger
 
 router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
+    """Main menu"""
     sms = "hihi"
+    logger.info("Выкатываем главное меню.")
     await message.answer(sms, reply_markup=menu_kb(message.from_user.id))
+
+@router.callback_query(F.data == "home")
+async def cmd_back_to_home(call: CallbackQuery):
+    """Return to main menu"""
+    await call.answer()
+    logger.info("Вернулись в главное меню.")
+    await call.message.edit_text(
+        "Вы вернулись в главное меню 🤸🏻",
+        reply_markup=menu_kb(call.from_user.id)
+    )
