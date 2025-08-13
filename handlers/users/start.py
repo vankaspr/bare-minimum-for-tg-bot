3851a75c137baf7e5c08ce06abb06fa26fb95a92
vankaspr <msvankaspr@gmail.com>
2025-08-13 18:20:00 +0500

    📌 add menu keyboard to handler

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -2,9 +2,11 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from keyboards import menu_kb
+
 router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     sms = "hihi"
-    await message.answer(sms)
+    await message.answer(sms, reply_markup=menu_kb(message.from_user.id))
