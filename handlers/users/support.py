b6d821ce292540612d45b4325dbf9978e2aa08dc
vankaspr <msvankaspr@gmail.com>
2025-08-13 23:21:09 +0500

    📌 create handler for command and callback support

--- handlers/users/support.py
+++ handlers/users/support.py
@@ -1,16 +1,13 @@
 """
     Handler for command --> /support
 """
-from datetime import datetime
+
+from typing import Union
 from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
-from aiogram.types import Message
-
-from config import support_group_id
-
-from settings import bot
-from settings.middlewares import logger
+from aiogram.types import Message, CallbackQuery
+from services import process_support_message
 
 support_router = Router()
 
@@ -20,37 +17,28 @@ class SupportForm(StatesGroup):
 
 
 @support_router.message(F.text.lower() == "/support")
+@support_router.callback_query(F.data == "menu:support")
 async def cmd_process_support_request(
-        message: Message,
+        event: Union[Message, CallbackQuery],
         state: FSMContext
 ):
+    """Handle /support command"""
+    if isinstance(event, CallbackQuery):
+        await event.answer()
+        message = event.message
+    else:
+        message = event
+
     await state.set_state(SupportForm.waiting_for_issue)
-    await message.answer("Опишите вашу проблему в одном сообщении.")
+    await message.answer(
+        "Опишите вашу проблему в одном сообщении."
+    )
 
 
 @support_router.message(F.text, SupportForm.waiting_for_issue)
 async def process_support(
         message: Message,
-        state: FSMContext
+        state: FSMContext,
 ):
-    user_issue = message.text
-    current_time = datetime.now().strftime("%d.%m.%Y, %H:%M")
-    logger.info("Штопаем саппорт-клаву, получаем сообщение в саппорт-канал")
-    await message.answer(
-        "Перенаправили ваш запрос в тех.поддержку!\n"
-        "Решим проблему как только, так сразу 😇",
-    )
-
-
-    await bot.send_message(
-        support_group_id,
-        f"⚠️ <b>Support</b> | New request\n\n"
-        f"👤 From: @{message.from_user.username}\n\n"
-        f"🆔 : <code>{message.from_user.id}</code>\n\n"
-        f"📅 Date: {current_time}\n\n"
-        f"<b>📩 Issue:</b>\n\n"
-        f"<blockquote>{user_issue}</blockquote>",
-        parse_mode="HTML"
-    )
-
+    await process_support_message(message)
     await state.clear()
