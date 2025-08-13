8d28553b4d5c6b5f36ab4b2688aaae32bf20369f
vankaspr <msvankaspr@gmail.com>
2025-08-13 16:07:35 +0500

    📌 create support router

--- core/routers/__init__.py
+++ core/routers/__init__.py
@@ -1,5 +1,7 @@
 from .start import router
+from .support import support_router
 
 __all__ = [
     "router",
+    "support_router"
 ]
---
+++ core/routers/support.py
@@ -0,0 +1,50 @@
+"""
+    Handler for command --> /support
+"""
+from datetime import datetime
+from aiogram import Router, F
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message
+
+from core.config import bot, support_group_id
+
+support_router = Router()
+
+
+class SupportForm(StatesGroup):
+    waiting_for_issue = State()
+
+
+@support_router.message(F.text.lower() == "/support")
+async def cmd_process_support_request(
+        message: Message,
+        state: FSMContext
+):
+    await state.set_state(SupportForm.waiting_for_issue)
+    await message.answer("Опишите вашу проблему в одном сообщении.")
+
+
+@support_router.message(F.text, SupportForm.waiting_for_issue)
+async def process_support(
+        message: Message,
+        state: FSMContext
+):
+    user_issue = message.text
+    current_time = datetime.now().strftime("%d.%m.%Y, %H:%M")
+    await message.answer("Перенаправили ваш запрос в тех.поддержку!\n"
+                         "Решим проблему как только, так сразу 😇")
+
+
+    await bot.send_message(
+        support_group_id,
+        f"⚠️ <b>Support</b> | New request\n\n"
+        f"👤 From: @{message.from_user.username}\n\n"
+        f"🆔 : <code>{message.from_user.id}</code>\n\n"
+        F"📅 Date: {current_time}\n\n"
+        f"<b>📩 Issue:</b>\n\n"
+        f"<blockquote>{user_issue}</blockquote>",
+        parse_mode="HTML"
+    )
+
+    await state.clear()
