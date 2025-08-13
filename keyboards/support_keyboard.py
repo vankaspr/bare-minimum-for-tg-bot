a8ebdc802e9239a79e64a4f3ef2d28636b1de90f
vankaspr <msvankaspr@gmail.com>
2025-08-14 00:50:33 +0500

    ♻️ separate support kb

---
+++ keyboards/support_keyboard.py
@@ -0,0 +1,12 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from services import add_back_to_home_button
+
+
+def support_kb():
+    menu = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="Есть ещё проблемы?", callback_data="menu:support")],
+        ]
+    )
+    return add_back_to_home_button(menu)
