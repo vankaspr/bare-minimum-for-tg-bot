cfb9ce86fb533c5cceb98b46ad1cf4782a345d05
vankaspr <msvankaspr@gmail.com>
2025-08-13 23:20:20 +0500

    ♻️ fix callback_data for keyboards

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,7 +1,6 @@
-from .menu_keyboard import menu_kb
+from .menu_keyboard import menu_kb, support_kb
 
 __all__ = [
     "menu_kb",
-
-
+    "support_kb",
 ]
--- keyboards/menu_keyboard.py
+++ keyboards/menu_keyboard.py
@@ -1,5 +1,4 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 from filters import IsAdmin
 from settings.middlewares import logger
 
@@ -7,23 +6,24 @@ from settings.middlewares import logger
 def menu_kb(user_id) -> InlineKeyboardMarkup:
     menu = InlineKeyboardMarkup(
         inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text='ТехПод ⚙️',
-                    callback_data="support_request"
-                )
-            ]
+            [InlineKeyboardButton(text='ТехПод ⚙️',callback_data="menu:support")],
         ]
     )
 
     if IsAdmin(user_id):
         logger.info(f"Батя в здании, допаем красную кнопку.")
         menu.inline_keyboard.append(
-            [InlineKeyboardButton(
-                text='🎀 Админ-панель 🎀',
-                callback_data='admin'
-            )]
+            [InlineKeyboardButton(text='🎀 Админ-панель 🎀',callback_data='menu:admin')]
         )
 
     return menu
 
+
+def support_kb():
+    menu = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="Есть ещё проблемы?", callback_data="menu:support")],
+            [InlineKeyboardButton(text="Вернуться в меню", callback_data="menu:home")]
+        ]
+    )
+    return menu
