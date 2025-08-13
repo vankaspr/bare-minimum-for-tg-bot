a87e6f361cb99b416719fb813d22046c3133aa09
vankaspr <msvankaspr@gmail.com>
2025-08-13 23:20:40 +0500

    📌 create handler for admin (demo)

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,7 +1,7 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message, CallbackQuery
-
+from filters import IsAdmin
 from keyboards import menu_kb
 from settings.middlewares import logger
 
@@ -14,7 +14,7 @@ async def cmd_start(message: Message):
     logger.info("Выкатываем главное меню.")
     await message.answer(sms, reply_markup=menu_kb(message.from_user.id))
 
-@router.callback_query(F.data == "home")
+@router.callback_query(F.data == "menu:home")
 async def cmd_back_to_home(call: CallbackQuery):
     """Return to main menu"""
     await call.answer()
@@ -22,4 +22,16 @@ async def cmd_back_to_home(call: CallbackQuery):
     await call.message.edit_text(
         "Вы вернулись в главное меню 🤸🏻",
         reply_markup=menu_kb(call.from_user.id)
+    )
+
+@router.callback_query(F.data == "menu:admin")
+async def admin(callback: CallbackQuery):
+    """Get admin keyboards"""
+    if not IsAdmin(callback.from_user.id):
+        await callback.answer("Доступ запрещен", show_alert=True)
+        return
+    await callback.answer()
+    await callback.message.answer(
+        "Админ-панель...\n"
+        "В разработке...⛏️"
     )
