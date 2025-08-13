3fb2089e2b40eefe0c6fbaa526d369accc46f0d5
vankaspr <msvankaspr@gmail.com>
2025-08-13 18:19:33 +0500

    📌 create basic inline keyboard

---
+++ keyboards/menu_keyboard.py
@@ -0,0 +1,22 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from filters import IsAdmin
+from settings.middlewares import logger
+
+
+def menu_kb(user_id) -> InlineKeyboardMarkup:
+    menu = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text='ТехПод ⚙️', switch_inline_query_current_chat='/support')
+            ]
+        ]
+    )
+
+    if IsAdmin(user_id):
+        logger.info(f"Батя в здании, допаем красную кнопку...")
+        menu.inline_keyboard.append(
+            [InlineKeyboardButton(text='🎀 Админ-панель 🎀', callback_data='admin')]
+        )
+
+    return menu
