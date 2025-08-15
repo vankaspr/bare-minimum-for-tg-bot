dd58043b40712e95e8bee98779547749dafd37b0
vankaspr <msvankaspr@gmail.com>
2025-08-15 17:54:28 +0500

    📌 add db to main

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import asyncio
 
+from database import engine, Base
 from handlers.admin import admin_router, auxiliary_router
 from handlers.users import router, support_router
 from settings import dp, bot
 from settings.middlewares import logger
 from utilities import set_commands
 
+async def init_database():
+    try:
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+        logger.info("✅ Таблицы БД успешно созданы/проверены")
+    except Exception as e:
+        logger.critical(f"❌ Ошибка подключения к БД: {e}")
+        raise
+
 
 async def main():
+
+    await init_database()
+
     dp.include_router(router=router)
     dp.include_router(router=support_router)
     dp.include_router(router=admin_router)
     dp.include_router(router=auxiliary_router)
+
     await set_commands()
+
     await bot.delete_webhook(drop_pending_updates=True)
+
+    logger.info("✅ Бот успешно запущен!")
     await dp.start_polling(bot)
 
 
@@ -22,7 +39,6 @@ if __name__ == "__main__":
     logger.info("🔁 Бот запускается...")
 
     try:
-        logger.info("✅ Бот успешно запущен!")
         asyncio.run(main())
     except Exception as e:
         logger.error(f"🆘 Ошибка при запуске бота: {e}")
