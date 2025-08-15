eabc9e590b325dd74f1dc25ecf660bcbaf9e789b
vankaspr <msvankaspr@gmail.com>
2025-08-15 16:41:40 +0500

    📌 create add back button for admin-home + create func for add only whatever button

--- services/__init__.py
+++ services/__init__.py
@@ -1,7 +1,9 @@
 from .services_for_support import process_support_message
-from .add_back_button import add_back_to_home_button
+from .add_back_button import add_back_to_home_button, add_back_to_admin_button, add_only_back_button
 
 __all__ = [
     "process_support_message",
-    "add_back_to_home_button"
+    "add_back_to_home_button",
+    "add_back_to_admin_button",
+    "add_only_back_button"
 ]
--- services/add_back_button.py
+++ services/add_back_button.py
@@ -11,3 +11,28 @@ def add_back_to_home_button(
         [InlineKeyboardButton(text=text, callback_data=callback_data)]
     )
     return keyboard
+
+
+def add_back_to_admin_button(
+        keyboard: InlineKeyboardMarkup,
+        text: str = "← Отмена",
+        callback_data: str = "admin:admin"
+) -> InlineKeyboardMarkup:
+    """ADD button back to admin menu"""
+    keyboard.inline_keyboard.append(
+        [InlineKeyboardButton(text=text, callback_data=callback_data)]
+    )
+    return keyboard
+
+
+def add_only_back_button(
+        text: str = "← Назад в меню",
+        callback_data: str = "menu:home"
+):
+    """Create one button to back to home or admin or whatever"""
+    back = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text=text, callback_data=callback_data)]
+        ]
+    )
+    return back
