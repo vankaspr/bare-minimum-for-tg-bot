aa82c99d767f7cbb0324ada0abcd5f80c891c501
vankaspr <msvankaspr@gmail.com>
2025-08-15 16:43:16 +0500

    📌 create new handlers for admin:users

--- handlers/admin/admin_panel.py
+++ handlers/admin/admin_panel.py
@@ -1,10 +1,13 @@
 """
     Handler for command --> /admin
     Handler for callback --> admin:admin
-    Handler for callback --> admin:stats
-    Handler for callback --> admin:payments
     Handler for callback --> admin:users
+                                --> admin:found_user
+                                --> admin:user_stats
+                                --> admin:active_ban
+                                --> admin:user_ban
     Handler for callback --> admin:error_logs
+    Handler for callback --> admin:payments
     Handler for callback --> admin:broadcast
 """
 
@@ -12,56 +15,98 @@ from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup
 from filters.is_admin import IsAdmin
 from keyboards import admin_kb, users_kb
-from services import add_back_to_home_button
+from keyboards.admin_keyboard import search_user_kb
+from services import add_back_to_home_button, add_only_back_button
 from settings.middlewares import logger
 from config import admin
 from utilities.error_logs import get_error_logs
+
 
 admin_router = Router()
 admin_router.callback_query.filter(IsAdmin(admin))
 
 
+# --- Главное меню ---
 @admin_router.callback_query(F.data == "admin:admin")
-async def admin(callback: CallbackQuery):
-    """Get admin keyboards"""
-    await callback.answer()
-    logger.info("Получении админ-панели с колбэка")
-    await callback.message.answer(
-        "Админ-панель...\n"
-        "В разработке...⛏️",
-        reply_markup=admin_kb()
-    )
-
-
 @admin_router.message(F.text == "/admin")
-async def admin_panel(message: Message):
-    logger.info("Получении админ-панели с команды")
+async def admin(update: Message | CallbackQuery):
+    """Get admin keyboards"""
+    if isinstance(update, CallbackQuery):
+        logger.info("Получении админ-панели с колбэка")
+        await update.answer()
+        message = update.message
+    else:
+        logger.info("Получении админ-панели с команды")
+        message = update
 
-    await message.answer(
-        "Админ-панель...\n"
-        "В разработке...⛏️",
-        reply_markup=admin_kb()
-    )
+    await message.answer("Админ-панель ⛏️",reply_markup=admin_kb())
 
-# TODO: Сделать обработчики для админских калбэков + всю логику к ним
-@admin_router.callback_query(F.data == "admin:stats")
-async def get_admin_stats(callback: CallbackQuery):
 
-    await callback.answer()
-    logger.info("Получении статистики с колбэка")
-    await callback.message.answer(
-        "Здесь будет лежать статистика",
-    )
-
+# --- Раздел пользователей ---
 @admin_router.callback_query(F.data == "admin:users")
 async def get_admin_users(callback: CallbackQuery):
 
     await callback.answer()
     logger.info("Получении раздела о пользователях с колбэка")
     await callback.message.answer(
-        "Здесь будет лежать ещё одна клавиатура"
-        "Где будет всё про пользователей",
+        "🛠 Управление пользователями",
         reply_markup=users_kb()
+    )
+
+@admin_router.callback_query(F.data == "admin:found_user")
+async def found_user(callback: CallbackQuery):
+    await callback.answer()
+    await callback.message.answer(
+        "🔍 Выберите способ поиска:",
+        reply_markup=search_user_kb()
+    )
+
+
+@admin_router.callback_query(F.data == "admin:user_stats")
+async def get_stats(callback: CallbackQuery):
+    await callback.answer()
+    await callback.message.answer("Тут лежит статистика активности пользователей")
+
+
+@admin_router.callback_query(F.data == "admin:active_ban")
+async def get_active_ban(callback: CallbackQuery):
+    await callback.answer()
+    await callback.message.answer("Тут мы смотрим кого забанили")
+
+
+@admin_router.callback_query(F.data == "admin:user_ban")
+async def ban_user(callback: CallbackQuery):
+    await callback.answer()
+    await callback.message.answer("Тут мы баним блядей")
+
+
+# --- Системные функции ---
+@admin_router.callback_query(F.data == "admin:error_logs")
+async def get_admin_logs(callback: CallbackQuery):
+    await callback.answer("⏳ Загружаю логи...")
+
+    logs = await get_error_logs()
+
+    if not logs:
+        await callback.message.answer(
+            "📭 Нет свежих ошибок ERROR/WARNING/DEBUG",
+        )
+
+    await callback.message.answer(
+        f"<b>Последние ошибки:</b>\n"
+        f"<pre>{logs}</pre>",
+        parse_mode="HTML",
+        reply_markup=add_only_back_button(text="← Отмена", callback_data="admin:admin")
+    )
+# --- Дополнительные разделы ---
+@admin_router.callback_query(F.data == "admin:payments")
+async def get_admin_payments(callback: CallbackQuery):
+
+    await callback.answer()
+    logger.info("Получении информации о платежах с колбэка")
+    await callback.message.answer(
+        "Здесь будет лежать инфа о платежах"
+        "На случай если я включу платежи",
     )
 
 @admin_router.callback_query(F.data == "admin:broadcast")
@@ -71,35 +116,4 @@ async def get_admin_broadcast(callback: CallbackQuery):
     logger.info("Создать рассылку с колбэка")
     await callback.message.answer(
         "Здесь будет лежать приблуда для создания рассылок",
-    )
-
-@admin_router.callback_query(F.data == "admin:error_logs")
-async def get_admin_logs(callback: CallbackQuery):
-    await callback.answer("⏳ Загружаю логи...")
-
-    logs = await get_error_logs()
-    back = InlineKeyboardMarkup(inline_keyboard=[])
-
-    if not logs:
-        await callback.message.answer(
-            "📭 Нет свежих ошибок ERROR/WARNING/DEBUG",
-            reply_markup=add_back_to_home_button(back)
-        )
-    else:
-        await callback.message.answer(
-            f"<b>Последние ошибки:</b>\n"
-            f"<pre>{logs}</pre>",
-            parse_mode="HTML",
-            reply_markup=add_back_to_home_button(back)
-        )
-
-
-@admin_router.callback_query(F.data == "admin:payments")
-async def get_admin_payments(callback: CallbackQuery):
-
-    await callback.answer()
-    logger.info("Получении информации о платежах с колбэка")
-    await callback.message.answer(
-        "Здесь будет лежать инфа о платежах"
-        "На случай если я включу платежи",
     )
--- keyboards/admin_keyboard.py
+++ keyboards/admin_keyboard.py
@@ -1,11 +1,10 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from services import add_back_to_home_button
+from services import add_back_to_home_button, add_back_to_admin_button
 
 
 def admin_kb():
     menu = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="📊 Статистика", callback_data="admin:stats")],
             [InlineKeyboardButton(text="🔨 Пользователи", callback_data="admin:users")],
             [InlineKeyboardButton(text="📢 Рассылка", callback_data="admin:broadcast")],
             [InlineKeyboardButton(text="🚨 Логи ошибок", callback_data="admin:error_logs")],
@@ -19,14 +18,24 @@ def users_kb():
     menu = InlineKeyboardMarkup(
         inline_keyboard=[
             [
-                InlineKeyboardButton(text="🔍 Найти гада", callback_data=f"admin:user_profile"),
-                InlineKeyboardButton(text="📊 Статистика", callback_data=f"admin:warn_user")
+                InlineKeyboardButton(text="🔍 Найти гада", callback_data="admin:found_user"),
+                InlineKeyboardButton(text="📊 Статистика", callback_data="admin:user_stats")
             ],
             [
-                InlineKeyboardButton(text="⚠️ Активные баны", callback_data=f"admin:timeout"),
-                InlineKeyboardButton(text="🚫 Забанить", callback_data=f"admin:user_stats")
+                InlineKeyboardButton(text="⚠️ Активные баны", callback_data="admin:active_ban"),
+                InlineKeyboardButton(text="🚫 Забанить", callback_data="admin:user_ban")
             ]
         ]
     )
 
-    return add_back_to_home_button(menu)
+    return add_back_to_admin_button(menu)
+
+def search_user_kb():
+    menu = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="По ID", callback_data="admin:search_by_id")],
+            [InlineKeyboardButton(text="По Username", callback_data="admin:search_by_username")]
+        ]
+    )
+
+    return add_back_to_admin_button(menu)
