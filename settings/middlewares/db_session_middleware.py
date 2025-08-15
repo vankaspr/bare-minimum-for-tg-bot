813c16f6326e4b051848bc8c2c137e78fb4ea1e5
vankaspr <msvankaspr@gmail.com>
2025-08-15 20:08:30 +0500

    📌 create middleware for DB

---
+++ settings/middlewares/db_session_middleware.py
@@ -0,0 +1,15 @@
+from typing import Callable, Awaitable, Any
+from aiogram import BaseMiddleware
+from database import SessionLocal
+
+
+class DBSessionMiddleware(BaseMiddleware):
+    async def __call__(
+            self,
+            handler: Callable[[Any, dict[str, Any]], Awaitable[Any]],
+            event: Any,
+            data: dict[str, Any]
+    ) -> Any:
+        async with SessionLocal() as session:
+            data["session"] = session
+            return await handler(event, data)
