e695212d7b21e8e45791369162d2df4c20351604
vankaspr <msvankaspr@gmail.com>
2025-08-15 17:53:48 +0500

    📌 create engine to db

---
+++ database/__init__.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import declarative_base
+from config import database_url
+
+db_dir = Path(__file__).parent.parent / "db"
+os.makedirs(db_dir, exist_ok=True)
+
+Base = declarative_base()
+
+engine = create_async_engine(
+    database_url,
+    echo=True,
+    connect_args={
+        "check_same_thread": False
+    }
+)
+
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
+
+async def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        await db.close()
