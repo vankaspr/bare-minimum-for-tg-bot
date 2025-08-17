b93f7da98560da6ba6d55f91d29f88f38f5aa8c2
vankaspr <msvankaspr@gmail.com>
2025-08-17 19:01:41 +0500

    📌 create const for add only back button (admin panel return)

--- services/__init__.py
+++ services/__init__.py
@@ -1,9 +1,10 @@
 from .services_for_support import process_support_message
 from .add_back_button import add_back_to_home_button, add_back_to_admin_button, add_only_back_button
-
+from .const import  BACK_BUTTON
 __all__ = [
     "process_support_message",
     "add_back_to_home_button",
     "add_back_to_admin_button",
-    "add_only_back_button"
+    "add_only_back_button",
+    BACK_BUTTON
 ]
